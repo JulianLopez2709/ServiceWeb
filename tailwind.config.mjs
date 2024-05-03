4b520817a0f26bffc7f7c57fa04b9fb32620450a
JulianLopez2709 <judalomor27.95@gmail.com>
2024-05-02 21:54:16 -0500

    favicon and image rendering

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	optimizeImages: {
+		eager: ['/src/assets/about-photo.png'],
+	},
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
